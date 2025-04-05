22689d06ac1d0661001fecc68162ca54cc3415cb
João Lucas <163215635+JoaoLucasmcS@users.noreply.github.com>
2025-04-05 11:33:31 -0300

    feat: adicionar estrutura do backend

---
+++ backend/BichoVacinado/src/main/java/com/BichoVacinado/BichoVacinado/BichoVacinadoApplication.java
@@ -0,0 +1,13 @@
+package com.BichoVacinado.BichoVacinado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BichoVacinadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BichoVacinadoApplication.class, args);
+	}
+
+}
---
+++ backend/BichoVacinado/src/test/java/com/BichoVacinado/BichoVacinado/BichoVacinadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BichoVacinado.BichoVacinado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BichoVacinadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
