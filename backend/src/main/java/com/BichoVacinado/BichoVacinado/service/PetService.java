8436f2b5e77a31bc7f7a3d0bf2873a360c261245
cauaojordao <cauaojordao@gmail.com>
2025-04-07 17:58:36 -0300

    fix: conserto na nomenclatura de um método

--- backend/src/main/java/com/BichoVacinado/BichoVacinado/service/PetService.java
+++ backend/src/main/java/com/BichoVacinado/BichoVacinado/service/PetService.java
@@ -21,19 +21,19 @@ public class PetService {
         pet.setEspecie(petRequest.getEspecie());
         pet.setRaca(petRequest.getRaca());
         pet.setHistoricoSaude(petRequest.getHistoricoSaude());
-        pet.setUsuarioID(petRequest.getUsuarioId());
+        pet.setUsuarioId(petRequest.getUsuarioId());
 
         Pet salvo = petRepository.save(pet);
 
         PetResponse response = new PetResponse();
-        response.setId(salvo.getID());
+        response.setId(salvo.getId());
         response.setNome(salvo.getNome());
         response.setEspecie(salvo.getEspecie());
         response.setRaca(salvo.getRaca());
         response.setIdade(salvo.getIdade());
         response.setPeso(salvo.getPeso());
         response.setHistoricoSaude(salvo.getHistoricoSaude());
-        response.setUsuarioId(salvo.getUsuarioID());
+        response.setUsuarioId(salvo.getUsuarioId());
 
         return response;
 
@@ -49,19 +49,19 @@ public class PetService {
         pet.setIdade(request.getIdade());
         pet.setPeso(request.getPeso());
         pet.setHistoricoSaude(request.getHistoricoSaude());
-        pet.setUsuarioID(request.getUsuarioId());
+        pet.setUsuarioId(request.getUsuarioId());
 
         Pet atualizado = petRepository.save(pet);
 
         PetResponse response = new PetResponse();
-        response.setId(atualizado.getID());
+        response.setId(atualizado.getId());
         response.setNome(atualizado.getNome());
         response.setEspecie(atualizado.getEspecie());
         response.setRaca(atualizado.getRaca());
         response.setIdade(atualizado.getIdade());
         response.setPeso(atualizado.getPeso());
         response.setHistoricoSaude(atualizado.getHistoricoSaude());
-        response.setUsuarioId(atualizado.getUsuarioID());
+        response.setUsuarioId(atualizado.getUsuarioId());
 
         
         return response;
